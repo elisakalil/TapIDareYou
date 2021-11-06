26c1acd37283f087a2b0fc9740d7e37e900f34ae
Elisa Kalil <elisakalilelias@gmail.com>
2021-11-06 18:32:26 -0300

    Initial commit

---
+++ IBOutlet_IBAction_IBOutletCollection/ViewController.swift
@@ -0,0 +1,43 @@
+
+//  ViewController.swift
+//  IBOutlet_IBAction_IBOutletCollection
+//  Created by Elisa Kalil on 19/10/21.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var buttons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for button in buttons {
+            
+            button.layer.cornerRadius = 12
+        }
+    }
+    
+
+    @IBAction func resettingDare(_ sender: UIButton) {
+        messageLabel.text = "Tap the buttons,\n I dare you"
+        messageLabel.textColor = .white
+        view.backgroundColor = .black
+    }
+    
+    @IBAction func changingTextCollor(_ sender: UIButton) {
+        messageLabel.text = "Tap the buttons,\n I dare you"
+        messageLabel.textColor = .green
+        view.backgroundColor = .black
+        
+        //array de UIColors e pegar pelo indice (random)
+    }
+    
+    @IBAction func acceptingDare(_ sender: UIButton) {
+        messageLabel.text = "☠"
+        view.backgroundColor = .red
+        
+    }
+}
+
---
+++ IBOutlet_IBAction_IBOutletCollectionTests/IBOutlet_IBAction_IBOutletCollectionTests.swift
@@ -0,0 +1,33 @@
+//
+//  IBOutlet_IBAction_IBOutletCollectionTests.swift
+//  IBOutlet_IBAction_IBOutletCollectionTests
+//
+//  Created by Elisa Kalil on 19/10/21.
+//
+
+import XCTest
+@testable import IBOutlet_IBAction_IBOutletCollection
+
+class IBOutlet_IBAction_IBOutletCollectionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
